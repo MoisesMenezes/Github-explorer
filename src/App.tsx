13d745884f2db2ec172c8966979738424e101b70
Moises Menezes <moisesmenezes90@gmail.com>
2020-04-30 01:21:38 -0300

    feat: add basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
-function App() {
-  return <h1>Hello world</h1>;
-}
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+};
 
 export default App;
