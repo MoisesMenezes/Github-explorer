00e846e0432512a5d62134a3ecf5b954f03a1183
Moises Menezes <moisesmenezes90@gmail.com>
2020-05-05 00:23:34 -0300

    feat: add styles on dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Input, Button, Repositories } from './styles';
+import LogoImg from '../../assets/logoImg.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={LogoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <Input placeholder="Digite o nome do repositório" />
+        <Button>Pesquisar</Button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/27028314?s=460&u=d841df66cc0899dee66eea05d5abe7122e87d49d&v=4"
+            alt="Moisés Menezes"
+          />
+          <div>
+            <strong>Node Js</strong>
+            <p>Aplicação em JS</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/27028314?s=460&u=d841df66cc0899dee66eea05d5abe7122e87d49d&v=4"
+            alt="Moisés Menezes"
+          />
+          <div>
+            <strong>Node Js</strong>
+            <p>Aplicação em JS</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,95 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
   color: #3a3a3a;
   font-size: 48px;
+  max-width: 450px;
+  line-height: 56px;
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+`;
+
+export const Button = styled.button`
+  width: 210px;
+  height: 70px;
+  background: #04d361;
+  border-radius: 0 5px 5px 0;
+  border: 0;
+  color: #fff;
+  font-weight: bold;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: ${shade(0.2, '#04d361')};
+  }
+`;
+
+export const Input = styled.input`
+  flex: 1;
+  color: #3a3a3a;
+  height: 70px;
+  padding: 0 24px;
+  border: 0;
+  border-radius: 5px 0 0 5px;
+
+  &::placeholder {
+    color: #a8a8b3;
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 18px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 65px;
+      height: 65px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
