3a0f54fb21674dcc08077333d7ba68bd645b5623
Moises Menezes <moisesmenezes90@gmail.com>
2020-05-07 20:59:28 -0300

    feat: add search repository on api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,42 +1,61 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 import { Title, Form, Input, Button, Repositories } from './styles';
 import LogoImg from '../../assets/logoImg.svg';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={LogoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
-      <Form>
-        <Input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <Input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <Button>Pesquisar</Button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/27028314?s=460&u=d841df66cc0899dee66eea05d5abe7122e87d49d&v=4"
-            alt="Moisés Menezes"
-          />
-          <div>
-            <strong>Node Js</strong>
-            <p>Aplicação em JS</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/27028314?s=460&u=d841df66cc0899dee66eea05d5abe7122e87d49d&v=4"
-            alt="Moisés Menezes"
-          />
-          <div>
-            <strong>Node Js</strong>
-            <p>Aplicação em JS</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a href="teste" key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
